c43c95ac1d9383f19c65aaf230d65f7d2bccdc07
Sofi Wesson <s201031@students.aie.edu.au>
2022-04-13 16:51:24 +1000

    proper movement, and ADS

--- URP Project/Assets/Scripts/Animation/AimOverrideControllerTPS.cs
+++ URP Project/Assets/Scripts/Animation/AimOverrideControllerTPS.cs
@@ -6,29 +6,53 @@ using Cinemachine;
 
 public class AimOverrideControllerTPS : MonoBehaviour
 {
+    [Header("Camera Data")]
+    [SerializeField]
+    [Tooltip("Camera used for aiming, set to control")]
     private CinemachineVirtualCamera m_aimCam = null;
+    [SerializeField]
+    [Tooltip("Camera's standard sensitivity")]
     private float m_camSensitivity;
+    [SerializeField]
+    [Tooltip("Camera's sensitivity when aiming")]
     private float m_aimCamSensitivity;
+
+    [Header("Layer Masking")]
+    [SerializeField]
+    [Tooltip("Layers to ignore")]
     private LayerMask m_aimLayerMask = new LayerMask();
-    private Transform m_aimCamTransform;
-
+    [Header("Bullet Data")]
+    [SerializeField]
+    [Tooltip("Provide a spawn position for a bullet")] // Shooting to be done
     private Transform m_bulletSpawnPos;
-    private GameObject m_bulletPrefab;
+    [SerializeField]
+    [Tooltip("Provide a bullet to be spawned")] // Shooting to be done
 
-    private PlayerInput m_controller;
+    private GameObject m_bulletPrefab;
+    private PlayerControlsTPS m_controller;
     private InputManagerTPS m_inputs;
     private Animator m_animator;
 
     private Vector2 m_currentAnimationVec;
     private Vector2 m_animationDirection;
 
+    [Space(20)]
+    [Header("Other")]
     [Tooltip("How far can the bullet be shot for our hitscan")]
     public float shotDistance = 200f;
+
+    [Tooltip("Use to control the rate at with the layers in the animation change.")]
+    public float animationLayerTransitionRate = 10f;
+
+    [Tooltip("Use to control how far it projects on a miss, useful for aiming via animation rigging")]
+    public float missedRaycastDistance = 200f;
+
+    public Transform m_aimMarkerTransform;
 
     // Start is called before the first frame update
     private void Awake()
     {
-        m_controller = GetComponent<PlayerInput>();
+        m_controller = GetComponent<PlayerControlsTPS>();
         m_inputs = GetComponent<InputManagerTPS>();
         m_animator = GetComponent<Animator>();
     }
@@ -37,13 +61,12 @@ public class AimOverrideControllerTPS : MonoBehaviour
     void Update()
     {
         Vector2 movement = m_inputs.move.normalized;
-        m_currentAnimationVec = Vector2.SmoothDamp(m_currentAnimationVec, movement, ref m_animationDirection, 0.1f, 1f);
-
-        m_animator.SetBool("IsStillADS", m_currentAnimationVec == Vector2.zero ? true : false);
+        m_currentAnimationVec = Vector2.SmoothDamp(m_currentAnimationVec,
+            movement, ref m_animationDirection, 0.1f, 1f);
+        m_animator.SetBool("IsStillADS", movement == Vector2.zero ? true : false);
         m_animator.SetBool("IsMotionADS", m_inputs.aim);
         m_animator.SetFloat("ForwardMotion", m_currentAnimationVec.y);
         m_animator.SetFloat("RightMotion", m_currentAnimationVec.x);
-
         Vector3 mouseWorldPos;
         Vector2 screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
         Ray ray = Camera.main.ScreenPointToRay(screenCenter);
@@ -54,11 +77,26 @@ public class AimOverrideControllerTPS : MonoBehaviour
             hitRay = raycastHit.transform;
         }
         else
-            mouseWorldPos = ray.GetPoint(20);
-    
+            mouseWorldPos = ray.GetPoint(missedRaycastDistance);
         if (m_inputs.aim)
         {
-            
+            m_aimCam.gameObject.SetActive(true);
+            m_controller.SetCamSensitivity(m_aimCamSensitivity);
+            m_controller.SetRotateOnMove(false);
+            m_animator.SetLayerWeight(1, Mathf.Lerp(m_animator.GetLayerWeight(1),
+                1f, Time.deltaTime * animationLayerTransitionRate));
+            Vector3 aimTarget = mouseWorldPos;
+            aimTarget.y = transform.position.y;
+            Vector3 aimDirection = (aimTarget - transform.position).normalized;
+            transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20);
+        }
+        else
+        {
+            m_aimCam.gameObject.SetActive(false);
+            m_controller.SetCamSensitivity(m_camSensitivity);
+            m_controller.SetRotateOnMove(true);
+            m_animator.SetLayerWeight(1, Mathf.Lerp(m_animator.GetLayerWeight(1),
+                0f, Time.deltaTime * animationLayerTransitionRate));
         }
     }
 }
